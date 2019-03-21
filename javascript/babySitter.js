287af7a4dcb5b5038a90559f3184cc82a3e7e98f
Ariana Baseman <ArianaMB@yahoo.com>
2019-03-21 12:56:41 -0400

    added jshint -- new issues

---
+++ javascript/babySitter.js
@@ -0,0 +1,19 @@
+'use strict';
+
+function babySitterCosts() {
+ let result = "";
+ if(A == 15) {
+   result = "15";
+ }
+ // if(num % 5 === 0 || num.toString().includes(5)) {
+ //   result += "buzz";
+ // }
+ // if(num % 7 === 0 || num.toString().includes(7)) {
+ //   result += "woof";
+ // }
+
+ return result ? result : A;
+
+}
+
+module.exports = babySitterCosts;
---
+++ spec/babySitter-test-spec.js
@@ -0,0 +1,28 @@
+const babySitterCosts = require('./babySitter');
+describe('babySitter', () => {
+
+  it('Family A should pay $15 before 11PM ', () => {
+    expect(babySitterCosts(familyA)).toBe(15);
+  });  
+  // it('9 should be fizz', () => {
+  //   expect(fizzBuzzWoof(9)).toBe("fizz");
+  // });
+  //  it('31 should be fizz', () => {
+  //   expect(fizzBuzzWoof(31)).toBe("fizz");
+  // });
+  //  it('10 should be Buzz', () => {
+  //   expect(fizzBuzzWoof(10)).toBe("buzz");
+  // });
+  //  it('35 should be fizzBuzzWoof', () => {
+  //   expect(fizzBuzzWoof(35)).toBe("fizzbuzzwoof");
+  // });
+  //  it('51 should be fizzBuzz', () => {
+  //   expect(fizzBuzzWoof(51)).toBe("fizzbuzz");
+  // });
+  //  it('14 should be Woof', () => {
+  //   expect(fizzBuzzWoof(14)).toBe("woof");
+  // });
+  //   it('71 should be Woof', () => {
+  //   expect(fizzBuzzWoof(71)).toBe("woof");
+  // });
+});
